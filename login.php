aeaf0eecdb27c373b88f15fe2f3159bcfe383eaf
Isadora-Gomes <isasilvacpv@gmail.com>
2025-10-14 11:42:14 -0300

    fonte login

--- login.php
+++ login.php
@@ -37,6 +37,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rethink+Sans:ital,wght@0,400..800;1,400..800&family=Space+Grotesk:wght@300..700&display=swap"
+    rel="stylesheet">
   <title>Login</title>
   <link rel="stylesheet" href="./css/login.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
