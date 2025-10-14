b04c50469bfba35e7df5299b24eca70ee49bae71
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-10-14 01:54:57 -0300

    banco com tuplas já criadas

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13/10/2025 às 02:39
+-- Tempo de geração: 14/10/2025 às 06:54
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -35,6 +35,14 @@ CREATE TABLE `comentarios` (
   `data_hora` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `id_usuario`, `id_produto`, `conteudo`, `data_hora`) VALUES
+(1, 2, 1, 'Não pode deixar no sol', '2025-10-14 01:37:38'),
+(2, 2, 1, 'Clientes amam', '2025-10-14 03:58:59');
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +58,15 @@ CREATE TABLE `movimentacoes` (
   `data_hora` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `movimentacoes`
+--
+
+INSERT INTO `movimentacoes` (`id`, `id_usuario`, `id_produto`, `quantidade`, `direcao`, `data_hora`) VALUES
+(1, 2, 1, 5, 'entrada', '2025-10-14 04:10:27'),
+(2, 2, 1, 13, 'saida', '2025-10-14 04:10:40'),
+(3, 2, 1, 80, 'entrada', '2025-10-14 04:20:58');
+
 -- --------------------------------------------------------
 
 --
@@ -64,6 +81,17 @@ CREATE TABLE `produtos` (
   `estoque` int(11) NOT NULL DEFAULT 0,
   `ativo` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `estoque`, `ativo`) VALUES
+(1, 'Pacote FINI Dentaduras', 'Balas de gelatina em formato de dentadura. Textura macia e elástica, com sabor frutado. Coloridas artificialmente. Produto industrializado.', 6.9, 92, 1),
+(2, 'Trufa da Chocolate', 'Produto à base de chocolate, com recheio cremoso interno e cobertura de chocolate ao leite. Textura macia no interior e casca firme externa.', 5.9, 146, 1),
+(3, 'BIS Chocolate Lacta', 'Wafer em camadas, coberto com chocolate ao leite. Textura crocante e leve. Produto industrializado, embalado em unidades pequenas.', 8.9, 82, 1),
+(4, 'Pacote Bala 7Belo', 'Bala sabor framboesa, de consistência firme e coloração rosada. Possui aroma característico e sabor artificial de fruta. Embalada individualmente.', 12, 42, 1),
+(5, 'Pacote Ouro Branco', 'Bombom com cobertura de chocolate branco e recheio cremoso à base de chocolate e castanha de caju. Textura crocante na casca e cremosa no interior.', 50, 87, 1);
 
 -- --------------------------------------------------------
 
@@ -85,7 +113,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `telefone`, `email`, `senha`, `admin`) VALUES
-(1, 'ADMIN', '12 99710-8249', 'admin@candystock.com', '$2y$10$98E67Rk2n/R.vfjzSn3nwuFOG/DNj4HiTYhJuU6iKxAdaftU0aQYe', 1);
+(1, 'ADMIN', '12 99710-8249', 'admin@candystock.com', '$2y$10$98E67Rk2n/R.vfjzSn3nwuFOG/DNj4HiTYhJuU6iKxAdaftU0aQYe', 1),
+(2, 'Lin', '12 99710-8249', 'lin@gmail.com', '$2y$10$MFW.0/nCjzCheSNWX0g1ee7rpqAcCLY8iTrdblfPIx9y03xJ2GK..', 0);
 
 --
 -- Índices para tabelas despejadas
@@ -127,25 +156,25 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `movimentacoes`
 --
 ALTER TABLE `movimentacoes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restrições para tabelas despejadas
